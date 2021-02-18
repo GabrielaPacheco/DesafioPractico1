64b2e3b39759e1f3a1997842830e59cd3f65abe3
GabrielaPacheco <ccgk02.gabriela.pacheco@gmail.com>
2021-02-17 21:26:21 -0600

    Update conversor.php

--- Desafio Practico 1/conversor.php
+++ Desafio Practico 1/conversor.php
@@ -16,50 +16,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"
         integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous">
     </script>
-    <style> 
-        .dropbtn { 
-            background-color: #4CAF50; 
-            color: white; 
-            padding: 16px; 
-            font-size: 16px; 
-            border: none; 
-            cursor: pointer; 
-        } 
-  
-        .dropdown { 
-            position: relative; 
-            display: inline-block; 
-        } 
-  
-        .dropdown-content { 
-            display: none; 
-            position: absolute; 
-            background-color: #f9f9f9; 
-            min-width: 160px; 
-            box-shadow: 0px 8px 16px  
-                0px rgba(0, 0, 0, 0.2); 
-            z-index: 1; 
-        } 
-  
-        .dropdown-content a { 
-            color: black; 
-            padding: 12px 16px; 
-            text-decoration: none; 
-            display: block; 
-        } 
-  
-        .dropdown-content a:hover { 
-            background-color: #f1f1f1 
-        } 
-  
-        .dropdown:hover .dropdown-content { 
-            display: block; 
-        } 
-  
-        .dropdown:hover .dropbtn { 
-            background-color: #3e8e41; 
-        } 
-    </style> 
 </head>
 
 <body style="background-color: pink; font-size: 25px; font-weight: bold;">
